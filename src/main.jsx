7ea949d47ca7e3dfe48dc990e12bcc543c16eac6
Ahnaf Abid <ahnafabid600@gmail.com>
2025-10-05 20:00:52 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from './root.jsx';
+import Home from './Home.jsx';
+import Mobiles from './Mobiles.jsx';
+import Laptops from './Laptops.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello fom react router</div>,
+    Component: Root,
+    children:[
+      {index: true, Component: Home},
+      {path: 'mobiles', Component: Mobiles},
+      {path: 'laptops', Component: Laptops}
+    ]
+  },
+  {
+    path: '/about',
+    element: <div>About me here</div>
+  },
+  {
+    path: 'blogs',
+    element: <div>All blogs are here</div>
+  },
+  {
+    path:'app',
+    Component: App
   },
 ]);
 
