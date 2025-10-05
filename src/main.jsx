b687f3515abf4a8cb61140be85b185140fcce044
Ahnaf Abid <ahnafabid600@gmail.com>
2025-10-05 23:39:17 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,6 +7,10 @@ import Root from './root.jsx';
 import Home from './Home.jsx';
 import Mobiles from './Mobiles.jsx';
 import Laptops from './Laptops.jsx';
+import Users from './Components/Users/Users.jsx';
+import Users2 from './Components/Users2/Users2.jsx';
+
+const usersPromise = fetch ('https://jsonplaceholder.typicode.com/users').then(res=> res.json())
 
 
 const router = createBrowserRouter([
@@ -16,7 +20,19 @@ const router = createBrowserRouter([
     children:[
       {index: true, Component: Home},
       {path: 'mobiles', Component: Mobiles},
-      {path: 'laptops', Component: Laptops}
+      {path: 'laptops', Component: Laptops},
+      {
+        path: 'users', 
+        loader:() => fetch('https://jsonplaceholder.typicode.com/users'),
+
+        Component: Users
+      },
+      {
+        path: "users2",
+        element: <Suspense fallback={<span>Loading...</span>}>
+          <Users2 usersPromise ={usersPromise}></Users2>
+        </Suspense>
+      }
     ]
   },
   {
